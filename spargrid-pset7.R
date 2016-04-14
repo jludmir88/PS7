9ddf8c428a7df7e58693602aa1b2e79619ba5ac3
Joseph Ludmir <jludmir@wustl.edu>
2016-04-14 04:15:30 -0500

    did parallel and dim code

--- spargrid-pset7.R
+++ spargrid-pset7.R
@@ -26,4 +26,38 @@ gx.sp <- aaply(nodes, 2, f, ..., .parallel=parallel)
 val.sp <- (gx.sp %*% weights)
 return(val.sp)
 }
+# For when dimension and parallel are different:
+# dim = dimensions, parallel running is now an option
+spargrid_int_dim_par <- function(g,..., lower, upper, dim, parallelCores = FALSE){
+# if parallel is true, run 6 cores on the computer
+if(parallel = TRUE){
+    require("doParallel")
+    registerDoParallel(cores = 6)
+  }
+  lower <- floor(lower)
+  upper <- ceiling(upper)
+# still throw an error if lower values are greater than upper ones
+if(any(lower >= upper)){
+    stop("The lower values must be smaller than the upper values.")
+}
+# Same formula as the function above, except now you are applying the integration
+# to each dimension at a time through a sequence
+  grid_eachdim <- function(i){
+    seq(lower[i], upper[i]-1, by=1)
+  }
+  grids <- as.matrix(expand.grid(llply(1:dim, gridFun, .parallel=parallel)))
+  
+  sp.grid <- createIntegrationGrid( 'KPU', dimension=dim, k=5 )
+  nodes<-grids[1,]+sp.grid$nodes
+  weights<-sp.grid$weights
+  for (i in 2:nrow(grids)){
+    nodes<-rbind(nodes,grids[i,]+sp.grid$nodes)  
+    weights<-c(weights,sp.grid$weights)
+  }
+  
+  gx.sp <- aaply(nodes, 2, f, ..., .parallel=parallel)
+  
+  val.sp <- (gx.sp%*%weights)
+  return(val.sp)
+}
 
